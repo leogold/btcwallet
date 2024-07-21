df880e92dba75b1bb7dfd610502330c8d95d9320
leogold <leogold.br@gmail.com>
2024-07-21 19:40:38 -0300

    Commit inicial

---
+++ src/createWallet.js
@@ -0,0 +1,33 @@
+//importando as dependencias
+const bip32 = require('bip32')
+const bip39 = require('bip39')
+const bitcoin = require('bitcoinjs-lib')
+
+//definir a rede
+//bitcoin --> rede principal --> mainnet
+//testnet --> rede de test --> testnet
+const network = bitcoin.networks.testnet
+
+//derivacao de carteiras HD
+const path = `m/49'/1'/0'/0`
+
+//criando o mneumonic para a seed (as palavras de senha)
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+//criando a raiz da carteira
+let root = bip32.fromSeed(seed, network)
+
+//criando uma conta --> par pvt-pub keys
+let account = root.derivePath(path)
+let node = account.derive(0).derive(0)
+
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network,
+}).address
+
+console.log("Carteira gerada")
+console.log("Endereço: ", btcAddress)
+console.log("Chave privada: ", node.toWIF())
+console.log("Seed", mnemonic)
